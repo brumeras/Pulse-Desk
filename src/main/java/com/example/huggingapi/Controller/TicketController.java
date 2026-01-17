4c1468d1c1b637c28a22c75b4126c22551e99cd9
Emilija Sankauskaite <semilija25@Emilijas-MacBook-Air.local>
2026-01-17 21:30:22 +0200

    Ticket Controller

---
+++ src/main/java/com/example/huggingapi/Controller/TicketController.java
@@ -0,0 +1,28 @@
+package com.example.huggingapi.Controller;
+
+import com.example.huggingapi.Logic.TicketService;
+import com.example.huggingapi.Model.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/tickets")
+public class TicketController
+{
+
+    @Autowired
+    private TicketService ticketService;
+
+    @GetMapping
+    public List<Ticket> getAllTickets()
+    {
+        return ticketService.getAllTickets();
+    }
+
+    @GetMapping("/{id}")
+    public Ticket getTicketById(@PathVariable Long id)
+    {
+        return ticketService.getTicketById(id).orElse(null);
+    }
+}
