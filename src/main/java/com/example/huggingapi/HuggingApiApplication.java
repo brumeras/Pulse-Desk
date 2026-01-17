59ef6c8e315ec9b8fc0026d65327abf23e5870b6
Emilija Sankauskaite <semilija25@Emilijas-MacBook-Air.local>
2026-01-17 15:46:20 +0200

    First commit

---
+++ src/main/java/com/example/huggingapi/HuggingApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.huggingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuggingApiApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HuggingApiApplication.class, args);
+        System.out.println("HuggingApiApplication started");
+    }
+
+}
---
+++ src/test/java/com/example/huggingapi/HuggingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huggingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuggingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
